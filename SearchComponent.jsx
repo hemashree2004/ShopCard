fb4665ec6bd0a62de476b9bacc58a81dc5c6a133
M Hemashree <137807977+hemashree2004@users.noreply.github.com>
2024-04-20 21:09:35 +0530

    Add files via upload

---
+++ SearchComponent.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+ 
+
+function SearchComponent({ searchCourse, courseSearchUserFunction }) {
+
+    return (
+
+        <header className="App-header">
+
+            <h1></h1>
+
+            
+
+        </header>
+
+    );
+}
+ 
+
+export default SearchComponent;
